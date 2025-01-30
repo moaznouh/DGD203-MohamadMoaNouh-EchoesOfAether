4da59dc6d9fc8d4f873c4a1c5a67e0d742845a24
moaznouh <83164100+moaznouh@users.noreply.github.com>
2025-01-30 04:50:01 +0300

    Add files via upload

---
+++ New folder/TextAdventureGame/TextAdventureGame/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace TextAdventureGame
+{
+    public class Item
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ New folder/TextAdventureGame/TextAdventureGame/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TextAdventureGame
+{
+    enum MenuOption { NewGame, Credits, Quit } 
+
+    class Menu
+    {
+        public MenuOption SelectedOption { get; private set; }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("\nWelcome to Echoes Of Aether !");
+            Console.WriteLine("1. New Game");
+     
+            Console.WriteLine("2. Credits");
+            Console.WriteLine("3. Quit");
+            Console.Write("\nSelect an option: ");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    SelectedOption = MenuOption.NewGame;
+                    break;
+           
+                case "2":
+                    SelectedOption = MenuOption.Credits;
+                    break;
+                case "3":
+                    SelectedOption = MenuOption.Quit;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    ShowMenu();
+                    break;
+            }
+        }
+    }
+}
